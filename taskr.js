2a57ef14ac428872d11fcdba660f83e10930c268
JS Robert Ouimet <jouimet@kivuto.com>
2018-02-28 11:55:49 -0500

    Added done and undo states.

--- taskr.js
+++ taskr.js
@@ -1,9 +1,10 @@
-function Task(id, name, dueDate, priority, done) {
+function Task(id, name, dueDate, priority) {
   this.id = id;
   this.name = name;
   this.dueDate = dueDate;
   this.priority = priority;
   this.done = false;
+  this.opacity = '1';
 }
 
 // Local Storage Methods
@@ -44,12 +45,45 @@ Store.prototype.genId = function() {
   return id;
 };
 
+Store.prototype.updateTasks = function(tasks) {
+  return localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
 Store.prototype.addTask = function(task) {
   const store = new Store();
   const tasks = store.getTasks();
 
   tasks.push(task);
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+  store.updateTasks(tasks);
+};
+
+// Refactor to toggle method!
+Store.prototype.checkmarkTask = function(id) {
+  const store = new Store();
+  const tasks = store.getTasks();
+
+  tasks.forEach(function(task) {
+    if(task.id === id) {
+      task.done = true;
+      task.opacity = '0.2';
+    }
+  });
+
+  store.updateTasks(tasks);
+};
+
+Store.prototype.undoTask = function(id) {
+  const store = new Store();
+  const tasks = store.getTasks();
+
+  tasks.forEach(function(task) {
+    if(task.id === id) {
+      task.done = false;
+      task.opacity = '1';
+    }
+  });
+
+  store.updateTasks(tasks);
 };
 
 Store.prototype.deleteTask = function(id) {
@@ -62,7 +96,7 @@ Store.prototype.deleteTask = function(id) {
     }
   });
 
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+  store.updateTasks(tasks);
 };
 
 // UI Methods
@@ -75,24 +109,47 @@ UI.prototype.displayTasks = function() {
   const tasks = store.getTasks();
 
   tasks.forEach(function(task) {
-    ui.listNewTask(task);
+    ui.addTask(task, task.opacity);
   });
 };
 
-UI.prototype.listNewTask = function(task) {
+UI.prototype.addTask = function(task, opacity) {
   const taskList = document.querySelector('.list-group.list-group-flush');
   const newTask = document.createElement('li');
 
+  let stateIcon;
+
   newTask.className = 'list-group-item';
+  newTask.style.opacity = opacity;
+
+  stateIcon = (task.done === false) ?
+    `<i class="fas fa-check mr-3 text-success"></i>` :
+    `<i class="fa fa-undo mr-3"></i>`;
+
   newTask.innerHTML = `
     <span style="display: none">${task.id}</span>
     <span>${task.name} <small class="text-muted ml-3">${task.dueDate}</small></span>
     <span class="float-right">
+      ${stateIcon}
       <i class="far fa-edit mr-3"></i>
       <i class="far fa-trash-alt text-danger"></i>
     </span>`;
 
   taskList.appendChild(newTask);
+};
+
+UI.prototype.checkmarkTask = function(target) {
+  const liElem = target.parentElement.parentElement;
+
+  target.className = 'fa fa-undo mr-3';
+  liElem.style.opacity = '0.2';
+};
+
+UI.prototype.undoTask = function(target) {
+  const liElem = target.parentElement.parentElement;
+
+  target.className = 'fa fa-check mr-3 text-success';
+  liElem.style.opacity = '1';
 };
 
 UI.prototype.editTask = function(target) {
@@ -106,7 +163,7 @@ UI.prototype.removeTask = function(target) {
 };
 
 // Add task
-document.getElementById('add-task').addEventListener('submit', function(e) {
+document.getElementById('add-task').addEventListener('submit', function() {
   const store = new Store();
   const ui = new UI();
 
@@ -119,21 +176,30 @@ document.getElementById('add-task').addEventListener('submit', function(e) {
   const task = new Task(store.genId(), taskName, dueDate, priority);
 
   store.addTask(task);
-  ui.listNewTask(task);
-
-  // e.preventDefault();
+  ui.addTask(task, task.opacity);
 });
 
-// Edit || delete
+// Checkmark, edit or delete task
 document.getElementById('task-list').addEventListener('click', function(e) {
   const store = new Store();
   const ui = new UI();
 
-  const edit = e.target.classList.contains('fa-edit');
-  const del = e.target.classList.contains('fa-trash-alt');
+  const [done, undo, edit, del, id] = [
+    e.target.classList.contains('fa-check'),
+    e.target.classList.contains('fa-undo'),
+    e.target.classList.contains('fa-edit'),
+    e.target.classList.contains('fa-trash-alt'),
+    e.target.parentElement.previousElementSibling.previousElementSibling.textContent
+  ];
 
-  const id = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
-
+  if(done) {
+    ui.checkmarkTask(e.target);
+    store.checkmarkTask(id);
+  }
+  if(undo) {
+    ui.undoTask(e.target);
+    store.undoTask(id);
+  }
   if(edit) {
     ui.editTask(e.target);
   }
